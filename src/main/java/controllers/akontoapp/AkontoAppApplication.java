68efd2de7dc47f833ab416b92e43499172745ddc
Miha <crnkovicmihovil@gmail.com>
2021-10-16 16:09:56 +0100

    Initial commit

---
+++ src/main/java/controllers/akontoapp/AkontoAppApplication.java
@@ -0,0 +1,13 @@
+package controllers.akontoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkontoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkontoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/controllers/akontoapp/AkontoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package controllers.akontoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkontoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

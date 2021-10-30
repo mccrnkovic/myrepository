c8369f23d889b379306048c187313df9fdb918f5
Miha <crnkovicmihovil@gmail.com>
2021-10-30 21:03:55 +0100

    added register form validation, validation for bank account needed

--- src/main/java/controllers/RegisterController.java
+++ src/main/java/controllers/RegisterController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -31,7 +34,10 @@ public class RegisterController {
     }
 
     @PostMapping
-    private String register(User user){
+    private String register(@Valid User user, Errors errors){
+        if(errors.hasErrors()){
+            return "/register";
+        }
         user.setAdmin(false);
         user.setPassword(BCryptPasswordEncoder.encode(user.getPassword()));
         log.info("Registering user: " + user);
--- src/main/java/model/Bill.java
+++ src/main/java/model/Bill.java
@@ -2,13 +2,22 @@ package model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import java.util.Date;
 
 @Data
+@Entity
 public class Bill {
+    @Id
     private Integer id;
+
+    @OneToOne
     private User buyer;
+    @OneToOne
     private User seller;
+
     private Double price;
     private Boolean paid;
     private Date whenPaid;
